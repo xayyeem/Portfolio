d536e7854cb0c492b130605fe383aecf2a61492c
DESKTOP-MTVPA3P\Khalid Jafri <kjafri30izback@gmail.com>
2026-07-11 20:58:30 +0530

    cors error fix

--- supabase/src/app.js
+++ supabase/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
